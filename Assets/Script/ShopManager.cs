b8b901cc68af81e4c8c008629c09cd4dea458d08
Sathya Narayanan <sathyanarayanansrini75@gmail.com>
2026-06-23 00:36:23 +0530

    Shop Menu Complete , Customize color and prefab in testing , gem later

---
+++ Assets/Script/ShopManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour
+{
+     public GameObject mainMenu;
+    public GameObject shopMenu;
+    public GameObject Shape;
+    public GameObject Trail;
+    public GameObject BG;
+
+    public void OpenShop()
+    {
+        mainMenu.SetActive(false);
+        shopMenu.SetActive(true);
+    }
+
+    public void CloseShop()
+    {
+        mainMenu.SetActive(true);
+        shopMenu.SetActive(false);
+    }
+
+    public void OpenShape()
+    {
+      Shape.SetActive(true);
+      BG.SetActive(false);
+      Trail.SetActive(false);
+    }
+
+    public void OpenBG()
+    {
+      BG.SetActive(true);
+      Shape.SetActive(false);
+      Trail.SetActive(false);
+    }
+
+    public void OpenTrails()
+    {
+      Trail.SetActive(true);
+      Shape.SetActive(false);
+      BG.SetActive(false);
+    }
+}
+
